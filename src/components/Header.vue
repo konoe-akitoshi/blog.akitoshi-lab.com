f55f6bac6d0552b887b9127d01f1fb7273d6f336
konoe-akitoshi <firsters.akitoshi@gmail.com>
2023-06-17 17:15:41 +0900

    fix: about page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,62 +15,67 @@ import { config } from '../settings/site-settings'
 -->
 
 <template>
-  <header class="header">
-      <div class="container">
-          <nuxt-link :to='`/`'>
-              <h1 class="site-title">
-                  {{ config.siteName }}
-              </h1>
-          </nuxt-link>
-          <nav class="main-nav">
-              <!--
+    <header class="header">
+        <div class="container">
+            <nuxt-link :to='`/`'>
+                <h1 class="site-title">
+                    {{ config.siteName }}
+                </h1>
+            </nuxt-link>
+            <nav class="main-nav">
+                <!--
               <li class="nav-item">
                   <a :href="config.social.repos" target="_blank" rel="noopener noreferrer">
                       GitHub
                   </a>
               </li>
               -->
-              <li class="nav-item">
-                  <nuxt-link :to='`/index`'>
-                      HOME
-                  </nuxt-link>
-              </li>
-              <li class="nav-item">
-                  <nuxt-link :to='`/about`'>
-                      ABOUT
-                  </nuxt-link>
-              </li>
-          </nav>
-      </div>
-  </header>
+                <li class="nav-item">
+                    <nuxt-link :to='`/`'>
+                        HOME
+                    </nuxt-link>
+                </li>
+                <li class="nav-item">
+                    <nuxt-link :to='`/about`'>
+                        ABOUT
+                    </nuxt-link>
+                </li>
+            </nav>
+        </div>
+    </header>
+    <div class="empty"></div>
 </template>
 
 <style scoped>
 .header {
-  padding: 10px 5% 10px;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  background-color: #101010;
-  display: flex;
-  align-items: center;
-  z-index: 10;
+    padding: 15px 10% 15px;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: #101010;
+    align-items: center;
+    z-index: 10;
 }
 
 .container {
-  max-width: 100%;
-  margin: 0 auto;
+    align-items: center;
+    right: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    max-width: 100%;
+    vertical-align: baseline;
 }
 
 .site-title {
-  font-size: 2rem;
-  color: #fff;
-  margin: 0;
-  padding: 0;
-  padding-bottom: 3px;
-  line-height: 1.4em;
-  justify-content: center;
-  letter-spacing: 1px;
+    font-size: 2rem;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+    padding-bottom: 3px;
+    line-height: 1.4em;
+    justify-content: center;
+    letter-spacing: 1px;
 }
 
 .main-nav {
@@ -78,20 +83,26 @@ import { config } from '../settings/site-settings'
     margin: 0;
     display: flex;
     align-items: center;
+}
 
-    .nav-item {
-        margin-right: 1.5rem;
-        font-size: var(--font-size-base);
-        font-weight: bold;
-        letter-spacing: 1px;
+.nav-item {
+    margin-right: 1.5rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #fff;
+    letter-spacing: 1px;
+}
+
+a {
+    color: #fff;
+
+    &:hover {
+        opacity: 0.6;
     }
+}
 
-    a {
-        color: var(--qlitre-colors-black);
-
-        &:hover {
-            opacity: 0.6;
-        }
-    }
+.empty {
+    height: 40px;
+    margin-bottom: 40px;
 }
 </style>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,80 +1,124 @@
 <script setup lang="ts">
-import { config } from '../settings/siteSettings';
+import { config } from '../settings/site-settings';
 
 const skillSet = [
-    { name: "VBA", since: "2019年06月" },
-    { name: "Python", since: "2020年01月" },
-    { name: "Django", since: "2021年01月" },
-    { name: "HTML / CSS / JavaScript", since: "2021年04月" },
-    { name: "Vue.js", since: "2021年06月" },
-    { name: "Nuxt.js", since: "2021年06月" },
-    { name: "TypeScript", since: "2022年04月" },
-    { name: "Django REST Framework", since: "2022年05月" },
-    { name: "React", since: "2022年07月" },
-    { name: "Next.js", since: "2022年07月" },
+    { name: "ロボット学会賞, デザイン賞", by: "RobocupJuniorJapanOpen", since: "2022" },
+    { name: "2nd", by: "Robocup Aisa-Apsific (Junior Soccer Open)", since: "2021" },
+    { name: "採択", by: "サイエンスキャッスル研究費 THK賞", since: "2021" },
+    { name: "RookieGameChangerAward", by: "FIRST Robotics Competition Infinite ReCharge", since: "2021" },
+    { name: "Best Team Description paper award, Individual 4th", by: "RoboCup Word Wide (Junior Soccer Open)", since: "2021" },
+    { name: "Open 2位", by: "RobocupJuniorJapanOpen", since: "2021" },
+    { name: "Open 準優勝", by: "ロボカップジュニア・京滋奈ブロック大会", since: "2021" },
+    { name: "2nd Prize", by: "RoboMaster", since: "2020" },
+    { name: "Most Popular Poster in Open", by: "RoboCupJunior Soccer Virtual Poster Session", since: "2020" },
+    { name: "入選3等「自律走行システムの考察-強いキーパー機を作るために」", by: "第64回日本学生科学賞", since: "2020" },
+    { name: "Open 優勝", by: "ロボカップジュニア・京滋奈ブロック大会", since: "2020" },
+    { name: "Open 準優勝", by: "関西オープン", since: "2019" },
+    { name: "LightWeight4位", by: "BIWAKOオープン", since: "2018" },
+    { name: "LightWeight4位,SuperTeam優勝", by: "せとうちオープン", since: "2018" },
+    { name: "NIPPON League サッカー ビギナーズ 予選12位", by: "ロボカップジュニア・ジャパンオープン2018和歌山", since: "2018" },
+    { name: "Classic部門 優勝", by: "SRC@国際ロボット展", since: "2017" },
+    { name: "全国予選 8位", by: "SRC13", since: "2016" },
+    { name: "2等賞「オクラに見つけた水玉の正体」", by: "第53回自然科学観察コンクール", since: "2012" },
 ]
 </script>
 
 <template>
     <main class="main">
-        <div class="image-area">
-            <img src="../assets/images/myprof.jpeg" alt="Qlitre">
+        <div class="profile-card">
+            <div class="card-after">
+                <div class="box">
+                    <div class="avatar">
+                        <img src="../assets/img/avatar.png" alt="Avatar">
+                    </div>
+                    <div class="info">
+                        <h1 class="name">Aktioshi Saeki</h1>
+                        <h2 class="abst">Engneer,Student</h2>
+                        <p class="desc">ロボットを作るのが大好きな大学生です．</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="about-content" style="margin-top:20px;">
-            <h2>
-                Qlitre
+        <div class="content">
+            <h2 class="content-sec">
+                受賞歴
             </h2>
-            <p class="p">
-                1989年生まれで株式会社で働いています。<br>
-                趣味、プログラミング、音楽を聴く。<br>
-                好きなアーティスト、柴田聡子さん。
-            </p>
-            <h2>
-                このサイトについて
-            </h2>
-            <p>
-                Pythonを中心にプログラミングの備忘録として書いています。<br>
-                Nuxt.js　+ microCMS + Netlifyで作成。<br>
-                2021年8月～
-            </p>
-            <h2>
-                お問い合わせ先
-            </h2>
-            <p>
-                <a :href="config.social.twitterProf" target="_blank" rel="noopener noreferrer">
-                    {{ config.social.twitter }}
-                </a>
-            </p>
-            <h2>
-                Github
-            </h2>
-            <p>
-                <a :href="config.social.github" target="_blank" rel="noopener noreferrer">{{ config.social.github }}</a>
-            </p>
-            <h2>
-                できること
-            </h2>
+            <hr class="content-sec-line" noshade="">
             <ul>
-                <li v-for="item in skillSet" :key="item.name">
+                <li class="prize" v-for="item in skillSet" :key="item.name">
                     <span>{{ item.since }}</span>
+                    <span class="skill-name">{{ item.by }}</span>
                     <span class="skill-name">{{ item.name }}</span>
                 </li>
             </ul>
-        </div>
-        <div style="text-align:center">
-            <TopToLink />
         </div>
     </main>
 </template>
   
 
-<style scoped lang="scss">
+<style scoped>
 .main {
     position: relative;
     width: 720px;
     margin: 0 auto 0;
     padding-top: 112px;
     padding-bottom: 50px;
+}
+
+.profile-card {
+    width: 100%;
+    background-image: url("../assets/img/back.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    border-radius: 10px;
+    margin-bottom: 40px;
+}
+
+.card-after {
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    ;
+    border-radius: 10px;
+
+}
+
+.box {
+    padding: 5% 5%;
+    display: flex;
+    justify-content: center;
+}
+
+.info {
+    margin-left: 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+}
+
+.name {
+    color: aliceblue;
+    font-size: 60px;
+    margin-bottom: 2%;
+}
+
+.abst {
+    color: aliceblue;
+    font-size: 20px;
+    margin-bottom: 5%;
+}
+
+.desc {
+    color: aliceblue;
+    font-size: 10px;
+}
+
+
+.avatar {
+    border-radius: 50%;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
 }
 
 @media (max-width: 1024px) {
@@ -89,11 +133,6 @@ const skillSet = [
     }
 }
 
-.image-area {
-    margin-top: 20px;
-    text-align: center;
-}
-
 img {
     width: 200px;
     height: 200px;
@@ -102,47 +141,36 @@ img {
     text-align: center;
 }
 
+.content {}
 
-.about-content {
-    position: relative;
-    max-width: 600px;
-    margin: 0 auto 0;
-    padding: var(--qlitre-space-4) var(--qlitre-space-8);
+
+.content-sec {
+    font-size: 36px;
+    color: #26272b;
+    margin-bottom: 10px;
 }
 
-.about-content {
-    p {
-        margin-top: 0;
-        margin-bottom: 2rem;
-        line-height: 1.8;
-        font-size: var(--font-size-base);
-        color: var(--qlitre-colors-gray-500);
-    }
-
-    h2 {
-        font-size: var(--font-size-xl);
-        margin-top: 3rem;
-        margin-bottom: 1rem;
-    } 
-
-    a {
-        color: var(--qlitre-colors-blue-500);
-    }
-
-    a:hover {
-        opacity: .5;
-    }
-
-    ul {
-        margin: 2rem 0;
-        line-height: 2;
-        font-size: var(--font-size-base);
-        color: var(--qlitre-colors-gray-500);
-        list-style-type: none;
-    }
+.content-sec-line{
+    color: #26272b;
+    margin-top: 0px;
+    margin-bottom: 20px;
 }
 
 .skill-name {
     margin-left: 1rem;
+}
+
+.prize {
+    margin-bottom: 10px;
+}
+
+hr {
+    display: block;
+    height: 2px;
+    padding: 0;
+    margin: 30px 0;
+    line-height: 0;
+    border: 0;
+    background-color: #3E4250;
 }
 </style>
