9c2ff609c7486ffc3d03d79e61effae24bab6280
konoe-akitoshi <firsters.akitoshi@gmail.com>
2023-06-23 14:23:57 +0900

    fix: ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ const { API_KEY, SERVICE_DOMAIN } = process.env;
 export default defineNuxtConfig({
   srcDir: "src/",
   ssr: true,
+  target: "server",
   runtimeConfig: {
     apiKey: API_KEY,
     serviceDomain: SERVICE_DOMAIN,
