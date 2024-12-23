0dd0bca0abe1aed64409ee760d87144b69e11c31
Akitoshi Saeki <51017077+konoe-akitoshi@users.noreply.github.com>
2024-12-24 06:28:31 +0900

    update: replace img with Image component for better optimization and enhance tag page message display

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -113,7 +113,7 @@ const PostDetail = ({ post }) => {
           remarkPlugins={[remarkGfm]}
           rehypePlugins={[rehypeRaw]}
           components={{
-            code({ node, inline, className, children, ...props }) {
+            code({ inline, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
                 <SyntaxHighlighter
