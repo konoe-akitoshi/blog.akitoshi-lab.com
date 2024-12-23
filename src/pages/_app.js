32a5ab4e4b28521c530da386bd480edc16d98e2c
Akitoshi Saeki <51017077+konoe-akitoshi@users.noreply.github.com>
2024-12-24 06:33:07 +0900

    update: comment out Navbar component in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Navbar from '../components/Navbar';
+// import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
